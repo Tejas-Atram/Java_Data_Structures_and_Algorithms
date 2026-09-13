600355350b7f0bd6ea12fecc2a8af4bcdb9c93a5
Tejas Atram <157953274+Tejas-Atram@users.noreply.github.com>
2026-09-13 23:51:51 +0530

    Improve comments for second largest element algorithm
    Updated comments to clarify the approach for finding the second largest element in an array.

---
+++ Arrays.java
@@ -0,0 +1,6 @@
+//Find the the second largest element in an Array
+
+// Brute force approach
+-At first we are going to sort the array in an ascending order.
+ -Then we are going to return the element in an array which is at the location arr[n-1] if we are starting from i=1.
+  -And then we are goinn to compare the largest element with the second largest element if(largest == secondLargest then traverse the array from the end of the array and get the element which is less than the largest element.)
