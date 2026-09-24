f909fdba69b75f563d9f09c6ede89552313c36cd
Tejas Atram <157953274+Tejas-Atram@users.noreply.github.com>
2026-09-25 00:24:21 +0530

    Add second largest element finder in Arrays.java
    Implement logic to find the second largest element in an array.

--- Arrays.java
+++ Arrays.java
@@ -4,3 +4,26 @@
 -At first we are going to sort the array in an ascending order.
  -Then we are going to return the element in an array which is at the location arr[n-1] if we are starting from i=1.
   -And then we are goinn to compare the largest element with the second largest element if(largest == secondLargest then traverse the array from the end of the array and get the element which is less than the largest element.)
+
+ -----------------------------------------------------------#Second Largest Element in the Array------------------------------------------------------------------------------------------------------------------------
+
+ public class Main
+{
+	public static void main(String[] args) {
+	    
+	    int arr[] = {1,4,6,33,76,88};
+	    int n = arr.length;
+	    int Largest = Integer.MIN_VALUE;
+	    int secondLargest = Integer.MIN_VALUE;
+	    
+	    for(int num: arr){
+	        if(num > Largest){
+	            secondLargest = Largest;
+	            Largest = num;
+	        }else if(num > secondLargest && num != Largest){
+	            throw new IllegalArgumentException("The array contains repeating elements ");
+	        }
+	    }
+		System.out.println("Second Largest Element in the Array is: " + secondLargest);
+	}
+}
